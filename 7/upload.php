204f36f5b5d5921f028bad60ffc3ba4f2f8a0d43
nyctophile0969 <nyctophile0969@proton.me>
2025-02-11 22:55:44 +0700

    apdet

--- 7/upload.php
+++ 7/upload.php
@@ -4,25 +4,27 @@
 <h3>Image File Upload Stats: </h3>
 
 <?php
-	if($_FILES["file"]["error"])
-	{
-		header("Location: index.html");
-		die();	
-	}
-	else{
+    if ($_FILES["file"]["error"]) {
+        header("Location: index.html");
+        die();
+    } else {
+        // Ambil ekstensi file asli
+        $fileExtension = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
 
-		echo "Name: ".$_FILES["file"]["name"];
-		echo "<br>Size: ".$_FILES["file"]["size"];
-		
-		move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/".$_FILES["file"]["name"]);
-		
-		echo "<br>File Uploaded to <a href='uploads/". $_FILES["file"]["name"] ."'>Here</a> ";
-	}
+        // Generate nama file random menggunakan uniqid() dan tambahkan ekstensi file
+        $randomFileName = uniqid() . "." . $fileExtension;
 
+        // Tampilkan informasi file
+        echo "Name: " . $_FILES["file"]["name"];
+        echo "<br>Size: " . $_FILES["file"]["size"];
 
+        // Pindahkan file ke folder uploads dengan nama file random
+        move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $randomFileName);
+
+        // Tampilkan link ke file yang diupload
+        echo "<br>File Uploaded to <a href='uploads/" . $randomFileName . "'>Here</a>";
+    }
 ?>
 
 </body>
 </html>
-
-
